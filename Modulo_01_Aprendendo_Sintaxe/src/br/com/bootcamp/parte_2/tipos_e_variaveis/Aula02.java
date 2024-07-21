57882f58d0da42bd334dd941452c7a4d186bedfa
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-21 13:51:32 -0300

    Tipos e Variáveis - 3

---
+++ Modulo_01_Aprendendo_Sintaxe/src/br/com/bootcamp/parte_2/tipos_e_variaveis/Aula02.java
@@ -0,0 +1,11 @@
+package br.com.bootcamp.parte_2.tipos_e_variaveis;
+
+public class Aula02 {
+
+
+	/*
+	 * Declaração de Variáveis
+	 * <Tipo> <nomeVariavel> <atribuicaoDeValorOpcional> 
+	 * */
+
+}
