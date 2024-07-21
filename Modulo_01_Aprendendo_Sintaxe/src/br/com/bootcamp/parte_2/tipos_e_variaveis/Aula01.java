38e6e4894640fd964fc7659eb14b6d4bb1058eec
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-21 13:51:17 -0300

    Tipos e Variáveis - 2

---
+++ Modulo_01_Aprendendo_Sintaxe/src/br/com/bootcamp/parte_2/tipos_e_variaveis/Aula01.java
@@ -0,0 +1,19 @@
+package br.com.bootcamp.parte_2.tipos_e_variaveis;
+
+/*Tipos Variaveis*/
+public class Aula01 {
+	
+	/**
+	 * Tipos Primitivos
+	 * - int
+	 * - byte
+	 * - short
+	 * - long
+	 * - float
+	 * - doble
+	 * - boolean
+	 * - char
+	 * Esses tipos não são considerados objetos.
+	 * E são armazenados diretamente na pilha de memória (Memory Stack)*/
+	
+}
