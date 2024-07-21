4bae6173d3f4a68e76c5ddd5454d5290867fb284
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-21 13:47:18 -0300

    Anatomia das Classes - 3

---
+++ Modulo_01_Aprendendo_Sintaxe/src/br/com/bootcamp/parte_1/anatomia/Aula01.java
@@ -0,0 +1,10 @@
+package br.com.bootcamp.parte_1.anatomia;
+
+/*Aula01 = MinhaClasse (Nome da Classe)*/
+public class Aula01 {
+
+	public static void main(String[] args) {
+		System.out.println("Olá turma, sejam bem-vindos!");
+	}
+
+}
