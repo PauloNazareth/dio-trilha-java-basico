a92210f7450e3acd93a39ed7f109b06852249d4d
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-23 22:59:08 -0300

    Estrutura de Repetição - For (Exemplo 2)

--- Modulo_03_Estrutura_Repeticao/src/br/com/bootcamp/parte_1/estrtutura_for/EstruturaFor.java
+++ Modulo_03_Estrutura_Repeticao/src/br/com/bootcamp/parte_1/estrtutura_for/EstruturaFor.java
@@ -2,4 +2,25 @@ package br.com.bootcamp.parte_1.estrtutura_for;
 
 public class EstruturaFor {
 
+	/**
+	 * #estrutura do controle de fluxo for
+	 * for (bloco de inicialização; expressão booleana de validação; bloco de atualização) {
+	 *      //comando que será executado até que a expressão de validação torne-se falsa 
+	 * }
+	 * */
+	
+	public static void main(String[] args) {
+		//Exemplo de Estrutura 1
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+		
+		//Exemplo de Estrutura 2
+		//O que importa é somente o bloco condicional
+		int carneirinhos = 1;
+		for( ; carneirinhos <=20; ) {
+		     System.out.println(carneirinhos + " - Carneirinho(s)");
+		     carneirinhos ++;
+		}
+	}
 }
