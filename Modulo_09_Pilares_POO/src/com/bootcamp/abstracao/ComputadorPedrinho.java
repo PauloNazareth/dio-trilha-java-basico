4fb5bd190332164c40b3e79034fb5fc995a77715
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-28 19:01:23 -0300

    Pilares POO - Exemplo Prático Polimorfismo

--- Modulo_09_Pilares_POO/src/com/bootcamp/abstracao/ComputadorPedrinho.java
+++ Modulo_09_Pilares_POO/src/com/bootcamp/abstracao/ComputadorPedrinho.java
@@ -3,20 +3,20 @@ package com.bootcamp.abstracao;
 public class ComputadorPedrinho {
 	public static void main(String[] args) {
 		
-		//Encapsulamento
-		MSNMessenger msn = new MSNMessenger();
-//		msn.validarConectadoInternet();
-		msn.enviarMensagem();
-		msn.receberMensagem();
-//		msn.salvarHistoricoMensagem();
+		//Polimorfismo
+		ServicoMensagemInstantanea smi = null;
 		
-		FacebookMessenger fbm = new FacebookMessenger();
-		fbm.enviarMensagem();
-		fbm.receberMensagem();
+		String appEscolhido = "msn";
 		
-		Telegram tlg = new Telegram();
-		tlg.enviarMensagem();
-		tlg.receberMensagem();
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		if(appEscolhido.equals("fmb"))
+			smi = new FacebookMessenger();
+		if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+		smi.enviarMensagem();
+		smi.receberMensagem();
 		
 	}
 }
--- Modulo_09_Pilares_POO/src/com/bootcamp/abstracao/ServicoMensagemInstantanea.java
+++ Modulo_09_Pilares_POO/src/com/bootcamp/abstracao/ServicoMensagemInstantanea.java
@@ -2,11 +2,11 @@ package com.bootcamp.abstracao;
 
 public abstract class ServicoMensagemInstantanea {
 	
-	public void validarConectadoInternet() {
+	protected void validarConectadoInternet() {
 		System.out.println("Validando se está conectado a internet");
 	}
 	
-	public void salvarHistoricoMensagem() {
+	protected void salvarHistoricoMensagem() {
 		System.out.println("Salvando o histórico da mensagem");
 	}
 	
