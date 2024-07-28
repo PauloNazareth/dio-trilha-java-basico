70c5606d69daeb4818b30c65d59e67fc7edaaee9
Paulo Nazareth <paulo.nazareth@live.com>
2024-07-28 12:49:10 -0300

    Fundamentos da Programação Orientada a Objetos em Java - Exemplo de Pacotes

---
+++ Modulo_08_Fundamentos_POO/src/com/mastersoft/clinal/model/Exame.java
@@ -0,0 +1,5 @@
+package com.mastersoft.clinal.model;
+
+public class Exame {
+
+}
---
+++ Modulo_08_Fundamentos_POO/src/com/mastersoft/clinal/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.mastersoft.clinal.service;
+
+import com.mastersoft.clinal.model.Exame;
+
+public class ExameService {
+
+	void salvarExeme(Exame exame) {}
+	
+}
