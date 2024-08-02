05fd410aa2f8c97f144fec9f751bf317e8b8178a
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-01 22:46:20 -0300

    Collections Java - Operações Básicas List

---
+++ Modulo_10_Colletions_Java/src/com/bootcamp/lists/operacoes_basicas/Tarefa.java
@@ -0,0 +1,15 @@
+package com.bootcamp.lists.operacoes_basicas;
+
+public class Tarefa {
+
+	private String descricao;
+
+	public Tarefa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
